6aa9cbcd0d8f84d7192f78e9104749c83129c5f4
Eva Evaldsson <eva.evaldsson2@ingka.ikea.com>
2023-02-03 14:00:58 +0100

    Updated with a second example

--- itertools_zip_longest.py
+++ itertools_zip_longest.py
@@ -7,3 +7,8 @@ b = [6,5,4,3,2,1]
 
 for a, b in it.zip_longest(a,b, fillvalue=0):
   print(a,b,a+b)
+
+# Example 2
+c = [1,2,3,4,5]
+d = [6,7,8,9,10,11,12,4]
+print([sum(s) for s in it.zip_longest(c,d,fillvalue=0)])
